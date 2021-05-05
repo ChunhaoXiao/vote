c02583b7357bd750a775c227b0ebfa3f9aa9de9d
Xiao <mail52@26.com>
2021-05-05 23:04:33 +0800

    test

--- app/Http/Controllers/Api/ReceivedVoteController.php
+++ app/Http/Controllers/Api/ReceivedVoteController.php
@@ -5,11 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Http\Resources\Movie;
 class ReceivedVoteController extends Controller
 {
     public function index() {
         $datas = Auth::user()->voteusers()->with(['video', 'user'])->paginate();
+        $datas->map(function($item){
+            $item->video = new Movie($item->movie);
+            
+        });
         return $datas;
 
     }
